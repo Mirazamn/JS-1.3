7411c775bd4b5db82eeecf6230e4f53970716c8c
Mirazam <165925890+Mirazamn@users.noreply.github.com>
2024-11-12 11:31:20 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+let a = Math.trunc(Math.random()*100)
+console.log(a);
+alert(`Sizda hozir Randm sonlar chiqishni boshledi`)
+alert(`Random Son: ${a}`)
+alert(`Biz Random sonni 2 ga ko'paytirmoqchimiz va bo'lmoqchimiz `)
+alert(`
+    Random orqali chiqqan son: ${a} edi
+    biz uni 2 ga ko'payytirib ${a*2} sonini chiqardik,
+    va shu sonni 2 ga bo'lib: ${a/2} sonini chiqardik,
+    va shu sonni 3 ga bo'lgandagi qoldiq: ${a%3} sonini chiqardik
+    `)
